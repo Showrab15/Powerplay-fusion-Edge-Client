b6c6a8d1364a9205f004e5cf28cd39907ffe9063
Showrab15 <supto50showrab@gmail.com>
2023-06-11 08:44:00 +0600

    modal added for feedback button

--- src/Pages/Dashboard/AdminDashboard/AllClasses/AllClassesTable.jsx
+++ src/Pages/Dashboard/AdminDashboard/AllClasses/AllClassesTable.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Swal from 'sweetalert2';
 
 const AllClassesTable = ({singleClass,index,refetch}) => {
-
+ 
 const {_id} = singleClass;
+
     const handleApproved = (id, status) => {
+
+
+
         console.log(id, status)
         fetch(`http://localhost:5000/addClasses/${id}/?status=${status}`, {
           method: 'PATCH'
@@ -13,7 +17,8 @@ const {_id} = singleClass;
           .then(data => {
             // console.log(data)
             if (data.modifiedCount) {
-              refetch()
+              refetch();
+              closeModal()
               Swal.fire({
                 position: 'top-end',
                 icon: 'success',
@@ -25,16 +30,18 @@ const {_id} = singleClass;
           })
     
       }
+     
+      
+      const showModal = () => {
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
 
 
-      const showModal = (id) => {
-
-        const modal = document.getElementById(`my_modal_${id}`);
-        if (modal) {
-          modal.showModal();
-        }
-      };
-
+const [modalOpen, setModalOpen] = useState(false);
       
       const sendFeedback = (event) => {
         event.preventDefault();
@@ -64,7 +71,7 @@ const {_id} = singleClass;
               showConfirmButton: false,
               timer: 1500
             })
-            onClose();}
+          }
         })
        
       };
@@ -92,25 +99,25 @@ const {_id} = singleClass;
                   <td>
                     <div className="flex flex-col gap-2   items-center space-x-3">
 
-                      <button onClick={() => handleApproved(singleClass._id, "approved")} disabled={singleClass.status === 'deny' || singleClass.status === 'approved'} className="btn btn-success btn-sm">Approve</button>
-                      <button className="btn  btn-error btn-sm" onClick={() => handleApproved(singleClass._id, "deny")} disabled={singleClass.status === 'deny' || singleClass.status === 'approved'} >deny</button>
+                      <button onClick={() => handleApproved(singleClass._id, "approved")} disabled={singleClass.status === 'deny' || singleClass.status === 'approved'} className="btn btn-success btn-xs">Approve</button>
+                      <button className="btn  btn-error btn-xs" onClick={() => handleApproved(singleClass._id, "deny")} disabled={singleClass.status === 'deny' || singleClass.status === 'approved'} >deny</button>
                       {/* Open the modal using ID.showModal() method */}
-                      <button className="btn" onClick={() => showModal(singleClass._id)}>open modal</button>
-                      <dialog id={`my_modal_${singleClass._id}`} className="modal">
-                        <form onSubmit={sendFeedback} method="dialog" className="modal-box">
-                          <input
+                      <button className="btn btn-xs" onClick={() => showModal(singleClass._id)}>open modal</button>
+                    <dialog id={`my_modal_${singleClass._id}`} open={modalOpen} className="modal">
 
-                            name='feedback'
-                            placeholder="Write your feedback here..."
-                            rows="4"
-                            className="w-full p-2 mt-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                          ></input>
-<input className="btn text-white bg-red-500 hover:bg-black" type="submit" value="feddback" />
-
-                        </form>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
-
-                      </dialog>
+                        <div method="dialog">
+                            <button className="btn btn-xs text-white bg-gray-500 hover:bg-black" onClick={closeModal}>✕</button>
+                            <form onSubmit={sendFeedback} className="modal-box">
+                                <input
+                                    name='feedback'
+                                    placeholder="Write your feedback here..."
+                                    rows="4"
+                                    className="w-full p-2 mt-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                ></input>
+                                <input className="btn text-white bg-red-500 hover:bg-black" type="submit" value="feedback" />
+                            </form>
+                        </div>
+                    </dialog>
                     </div>
                   </td>
 
